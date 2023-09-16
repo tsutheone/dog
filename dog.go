158b0eb9390cca687a21765c2223f1e052be4bad
meridianss <meridianss@WhiteThunder>
2023-09-16 10:41:33 +0200

    created functionality as a go module

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func DoggoGrowsUp(s string) string {
+	return "I am now a big dog!: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tsutheone/dog
+
+go 1.21.1
